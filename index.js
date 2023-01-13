ce9def38279a2e4b5b9129b2cf39b1d9e132376c
hari <dev.hainguyen92@gmail.com>
2023-01-13 17:40:20 +0700

    required colors

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const userRoutes = require('./routes/usersRoutes');
 const dotenv = require('dotenv');
 // const logger = require('./middleware/logger');
 const morgan = require('morgan');
+const colors = require('colors');
 const connectDB = require('./config/db');
 
 dotenv.config({
@@ -23,10 +24,10 @@ if(process.env.NODE_ENV === 'dev'){
 app.use('/api/users', userRoutes);
 
 const server = app.listen(PORT, () => {
-    console.log(`Server Started at ${PORT}`)
+    console.log(`Server Started at ${PORT}`.blue.bold);
 })
 
 process.on('unhandledRejection', (err, promise) => {
-    console.log(`Error: ${err.message}`);
+    console.log(`Error: ${err.message}`.red.bold);
     server.close(() => process.exit(1));
 })
