b7dc545077a80048738c1abe22d681a7c1aeb46d
hari <dev.hainguyen92@gmail.com>
2023-01-18 17:20:54 +0700

    add token middlware request

--- middleware/auth.js
+++ middleware/auth.js
@@ -25,7 +25,7 @@ exports.protect = asyncHandler(async (req, res, next) => {
 exports.authorize = (...roles) => {
     return (req, res, next) => {
         if(!roles.includes(req.user.roles)){
-            return next(new errorResponse(`User role ${req.user.role} is not authorized to access this route`, 401));
+            return next(new errorResponse(`User role ${req.user.role} is not authorized to access this route`, 403));
         }
         next();
     };
